5bac4599617122720b1b7c1fbc925a40332c644f
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-12-02 14:01:10 -0300

    feat: part 2 complete

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -2,6 +2,10 @@ import sys
 import re
 sys.path.append("..")  
 from utils import read_document
+red_cube_qty = 1
+green_cube_qty = 1
+blue_cube_qty = 1
+
 
 def split_game(game : str) -> list[str]:
     """summary: split document line into list of strings"""
@@ -26,10 +30,40 @@ def check_game(results : list[str]) -> bool:
             return False
     return True
 
+def set_red_power(value : int):
+    global red_cube_qty
+    if(red_cube_qty < value):
+        red_cube_qty = value
+
+def set_green_power(value : int):
+    global green_cube_qty
+    if(green_cube_qty < value):
+        green_cube_qty = value
+
+def set_blue_power(value : int):
+    global blue_cube_qty
+    if(blue_cube_qty < value):
+        blue_cube_qty = value
+
+def check_power(results : list[str]) -> bool:
+    cubes_options = {
+        "red": set_red_power,
+        "green": set_green_power,
+        "blue": set_blue_power
+    }
+    for result in results:
+        type_result = result.strip().split(" ")
+        if type_result[1] in cubes_options:
+            cubes_options[type_result[1]](int(type_result[0]))
+
+
+
 
 def play_game():
+    global red_cube_qty, green_cube_qty, blue_cube_qty
     lines = read_document("data.txt")
     id_sum = 0
+    power_sum = 0
     games = [line.strip().split(": ") for line in lines]
     for game in games:
         game_id = int(game[0].split(" ")[1])
@@ -40,13 +74,19 @@ def play_game():
         for value in values:
             if not check_game(value):
                 game_is_posible = False
-                break
+            check_power(value)
+        print(f"Game #{game_id}´s power => { red_cube_qty * green_cube_qty * blue_cube_qty}")
+        power_sum += red_cube_qty * green_cube_qty * blue_cube_qty
+        red_cube_qty = 0
+        green_cube_qty = 0
+        blue_cube_qty = 0
         if game_is_posible: 
             print(f"Game #{game_id} is possible")
             id_sum += game_id
         else:
             print(f"Game #{game_id} is not possible")
-    print(f"Sum of possible games is {id_sum}")
+    print(f"PART 1: Sum of possible games is {id_sum}")
+    print(f"PART 2: Sum of power is {power_sum}")
     
         
     
