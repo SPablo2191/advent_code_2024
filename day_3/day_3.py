28a60eb2c94a4c5ac839f57062dff48c86cf9f52
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-12-03 21:56:06 -0300

    feat: separe get_neighbors logic

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -7,13 +7,17 @@ symbol_list = [symbol for symbol in string.punctuation if symbol != "."] # get l
 
 def is_part_number(matrix, row, column) -> bool:
     global symbol_list
+    neighbors = get_neighbors(matrix, row, column)
+    # print(f"neighbors of {matrix[row][column]}: {neighbors}")
+    return any(element in symbol_list for element in neighbors)
+
+def get_neighbors(matrix, row, column):
     neighbors = []
     for i in range(row-1, row+2):
         for j in range(column-1, column+2):
             if 0 <= i < len(matrix) and 0 <= j < len(matrix[0]) and (i != row or j != column):
                 neighbors.append(matrix[i][j])
-    # print(f"neighbors of {matrix[row][column]}: {neighbors}")
-    return any(element in symbol_list for element in neighbors)
+    return neighbors
 
 
 def get_sum_part_numbers():
@@ -24,6 +28,8 @@ def get_sum_part_numbers():
         aux = ""
         cont_part_number = 0
         for j  in range(len(lines_matrix[i])):
+            if lines_matrix[i][j] == "*":
+                print("")
             if lines_matrix[i][j].isnumeric():
                 if is_part_number(lines_matrix, i,j):
                     cont_part_number += 1
