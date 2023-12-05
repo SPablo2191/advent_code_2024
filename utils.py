b457bd6888e5f7abe0ce9a5a8ec8742dd6880aec
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-12-05 18:04:14 -0300

    docs: update docs from read_document function

--- utils.py
+++ utils.py
@@ -1,4 +1,14 @@
 def read_document(file_name : str) -> list[str]:
+    """get the data from a file and return it as a list of strings
+    how to use it:
+    import sys
+    sys.path.append("..") 
+    from utils import read_document
+    Keyword arguments:
+    file_name -- string containing the name of the file to read
+    Return: list of lines from the file
+    """
+    
     with open(file_name, "r") as f:
         return f.readlines()
     
